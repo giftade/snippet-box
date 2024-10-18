93c0f3edcfa0ebcca16fe40249103dcfb1dc90b6
Segun Ade <giftsegunade@gmail.com>
2024-10-18 12:31:50 +0100

    initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+	"github.com/bmizerany/pat"
+)
+
+func (app *application) routes() http.Handler {
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	dynamicMiddleware := alice.New(app.session.Enable, noSurf, app.authenticate)
+	mux := pat.New()
+	mux.Get("/", dynamicMiddleware.ThenFunc(app.home))
+	mux.Get("/about", dynamicMiddleware.ThenFunc(app.about))
+	mux.Get("/snippet/create", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.createSnippet))
+	mux.Get("/snippet/:id", dynamicMiddleware.ThenFunc(app.showSnippet))
+
+	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
+	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
+	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
+	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
+	mux.Post("/user/logout", dynamicMiddleware.Append(app.requireAuthenticatedUser).ThenFunc(app.logoutUser))
+	mux.Get("/user/passwordReset", dynamicMiddleware.ThenFunc(app.passwordResetForm))
+	mux.Post("/user/passwordReset", dynamicMiddleware.ThenFunc(app.passwordReset))
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
+
+	return standardMiddleware.Then(mux) 
+}
